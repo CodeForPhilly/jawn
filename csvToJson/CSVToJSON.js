c78c72ecf32c060f1a620bce80f5f4d7fe11394c
Cameron Sima <Cameron Sima>
2016-03-02 13:34:25 -0500

    csv to json converter

---
+++ csvToJson/CSVToJSON.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var path = require('path');
+
+
+function lineToJSON(headings, lines) {
+	result = {};
+	for (var i = 0; i < headings.length; i++) {
+		// Remove vestigal double quotes from beginning
+		// and end of line strings
+		line = lines[i].replace(/^"|"$/g, '');
+		result[headings[i]] = line;
+	}
+	return result;
+}
+
+function convert(filepath) {
+	var fpath = path.join(__dirname, filepath);
+	var jsonArr = [];
+	var file = fs.readFileSync(fpath, 'utf8');
+
+	// Replace newlines to enforce the same style for newline
+	// characters across operating systems.
+	var lines = file.replace(/(\r\n|\r|\n)/g, '\n')
+		.split(/\r?\n/g);
+	var headings = lines.shift();
+	lines.forEach(function(line) {
+		if (line) {;
+			// positive lookahead regex to avoid splitting on commas
+			// that appear within double-quotes
+			jsonArr.push(lineToJSON(headings.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/), 
+									line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)));
+		}
+	});
+	return jsonArr;
+}
+module.exports = convert;
