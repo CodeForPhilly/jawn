91cb5188f7372442f91e7a9d6222311f2dcd2e64
Benjamin Ettori <Benjamin Ettori>
2016-03-02 18:33:48 -0500

    Update package.json and test files to run tests correctly

--- csvToJsonReader/tests/testreadFile.js
+++ csvToJsonReader/tests/testreadFile.js
@@ -3,7 +3,7 @@ var csvJson = require('../csvToJson');
 
 test('simpleFile', function(t) {
 	var expectedResult = ['{"header1":"one","header2":"two","header3":"three"}','{"header1":"four","header2":"five","header3":"six"}'];
-	var result = csvJson().readFile('../testfile.csv');
+	var result = csvJson().readFile('./csvToJsonReader/testfile.csv');
 	t.deepEqual(result, expectedResult);
 	t.end();
 });
