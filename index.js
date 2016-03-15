8fb35f2ef347915d413f003afe4b0d79fcee30d0
wtkm7 <wtkm7@users.noreply.github.com>
2016-03-15 19:19:23 -0400

    import a file

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-module.exports = Jawn
-function Jawn () {}
-Jawn.prototype.import = function (data) {
-  return 'Hello World'
-}
+var fs = require('fs');
+var path = require('path');
+var level = require('level');
+var hypercore = require('hypercore');
+
+module.exports = Jawn;
+
+function Jawn() {}
+
+Jawn.prototype.import = function (core, path) {
+
+    var ws = core.createWriteStream();
+
+    // read the file from disk and write it to the stream line by line
+    var lines = fs.readFileSync(path)
+      .toString()
+      .split(/\r?\n/g)
+    .filter(function (line) {
+        return line !== '';
+    });
+
+    lines.forEach(function (line) {
+        ws.write(line);
+    });
+
+    return ws;
+
+};
