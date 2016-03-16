6c3b64c4ad0378fc63334dfd38c644388de675cf
Matt Zumwalt <matt@databindery.com>
2016-03-15 21:38:10 -0400

    import from stream (incomplete) refs #3

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
-var fs = require('fs');
-var path = require('path');
-var level = require('level');
-var hypercore = require('hypercore');
+var level = require('level')
+var hypercore = require('hypercore')
 
-module.exports = Jawn;
+module.exports = Jawn
+function Jawn () {
+  var self = this
+  self.db = level('data.jawn')
+  self.core = hypercore(self.db)
+}
 
-function Jawn() {}
-
-Jawn.prototype.import = function (core, path) {
-
-    var ws = core.createWriteStream();
-
-    // read the file from disk and write it to the stream line by line
-    var lines = fs.readFileSync(path)
-      .toString()
-      .split(/\r?\n/g)
-    .filter(function (line) {
-        return line !== '';
-    });
-
-    lines.forEach(function (line) {
-        ws.write(line);
-    });
-
-    return ws;
-
-};
+Jawn.prototype.import = function (stream) {
+  var ws = this.core.createWriteStream()
+  stream.on('data', function (line) {
+    ws.write(line)
+  })
+  stream.on('end', function () {
+    ws.end(function () {
+      console.log(ws.id.toString('hex'))
+    })
+  })
+  return ws
+}
--- test/import.js
+++ test/import.js
@@ -1,56 +1,29 @@
-var fs = require('fs');
-var path = require('path');
-var tape = require('tape');
-var level = require('level');
-var hypercore = require('hypercore');
+var test = require('tape')
+var Jawn = require('../')
+var fs = require('fs')
 
-var Jawn = require('../');
+var j = new Jawn()
 
-tape('test file ingest', function (t) {
+test('import csv to jawn', function (t) {
+  var data = fs.createReadStream('./test/sample/sample.csv')
+  var ws = j.import(data)
+  var expected = [
+              {'Type of Experience': 'Writing software in any programming language', 'Little/No Experience': 1, 'Some Experience': 5, 'Very Familiar': 4},
+              {'Type of Experience': 'Frontend Web Development', 'Little/No Experience': 4, 'Some Experience': 3, 'Very Familiar': 3},
+              {'Type of Experience': 'Server-side (backend) Web Development', 'Little/No Experience': 4, 'Some Experience': 4, 'Very Familiar': 2},
+              {'Type of Experience': 'Using Git to track changes and share code (add, commit, push, pull)', 'Little/No Experience': 2, 'Some Experience': 5, 'Very Familiar': 3}
+  ]
+  ws.on('end', function () {
+    var feedId = ws.id.toString('hex')
+    var rs = j.core.createReadStream(feedId)
 
-  var db = level('test.db');
-  var core = hypercore(db);
+    rs.on('data', function (block) {
+      t.same(block.toString(), expected.shift(), 'block matches imported line')
+    })
 
-  var J = new Jawn();
+    var blocks = j.core.get(feedId).blocks
 
-  var path = './test.csv';
-
-  var ws = J.import(core, path);
-
-  var filecontents = fs.readFileSync(path)
-                        .toString()
-                        .split('\n')
-                        .filter(function (line) {
-                          return line !== '';
-                        });
-
-  // get number of rows in test.csv
-  var numrows = filecontents.length;
-
-  ws.end(function () {
-
-    var id = ws.id.toString('hex');
-
-    t.ok(ws.id, 'has id');
-
-    t.same(ws.blocks, numrows, 'there should be one row for each block');
-
-    // Now create a read stream with the same id and read the data
-    var rs = core.createReadStream(id);
-
-    var feedcontents = [];
-
-    rs.on('data', function (data) {
-      // gather feed contents
-      feedcontents.push(data.toString());
-    });
-
-    rs.on('end', function () {
-      t.same(filecontents, feedcontents, 'contents of feed should match file contents');
-    });
-
-  });
-
-  t.end();
-
-});
+    t.same(blocks, 4, 'correct number of blocks returned')
+    t.end()
+  })
+})
