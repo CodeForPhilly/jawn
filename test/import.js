07b552003242b04d3f64637108a305fbbc80d5c2
Matt Zumwalt <matt@databindery.com>
2016-03-18 16:02:54 -0700

    import files with importPipeline

--- test/import.js
+++ test/import.js
@@ -1,39 +1,61 @@
 var test = require('tape')
+var fs = require('fs')
+var path = require('path')
 var Jawn = require('../')
 var memdb = require('memdb')
 
 test('import json to jawn', function (t) {
   var jawn = freshJawn()
-  var importStream = jawn.createImportStream({'format': 'json'})
-  // Imitate the stream that would come from reading sample.csv
-  // importStream should parse the CSV correctly, identifying the first row as headers
-  // This is the same as doing
-  //    var data = fs.createReadStream('./test/data/sample.csv')
-  //    data.pipe(importStream)
-  // except the writes are being performed synchronously/inline so we can call importStream.end() after writing the contents into it.
-  importStream.write('{foo: "bar", name: "josie"}')
-  importStream.write('{foo: "baz", name: "eloise"}')
-  importStream.write('{foo: "baz", name: "francoise"}')
-
-  // This should be expecting JSON objects, not strings.
-  // temporarily expecting strings in order to hand off the code as-is
+  importFromFile(jawn, 'dummy.json', {'format': 'json'}, verify)
   var expected = [
-    '{foo: "bar", name: "josie"}',
-    '{foo: "baz", name: "eloise"}',
-    '{foo: "baz", name: "francoise"}'
+    '{"foo":"bar","name":"josie","age":"35"}',
+    '{"foo":"baz","name":"eloise","age":"71"}',
+    '{"foo":"baz","name":"francoise","age":"5"}'
   ]
-
-  importStream.end(function () {
-    var feedId = importStream.id.toString('hex')
+  function verify (err, feedId) {
+    if (err) { console.log(err) }
     var rs = jawn.core.createReadStream(feedId)
     rs.on('data', function (block) {
       t.same(block.toString(), expected.shift(), 'block matches imported line')
     })
     t.same(jawn.core.get(feedId).blocks, 3, 'correct number of blocks returned')
     t.end()
-  })
+  }
 })
+
+test('import csv to jawn', function (t) {
+  var jawn = freshJawn()
+  importFromFile(jawn, 'sample.csv', {'format': 'csv'}, verify)
+  var expected = [
+    '{"Type of Experience":"Writing software in any programming language","Little/No Experience":"1","Some Experience":"5","Very Familiar":"4"}',
+    '{"Type of Experience":"Frontend Web Development","Little/No Experience":"4","Some Experience":"3","Very Familiar":"3"}',
+    '{"Type of Experience":"Server-side (“backend”) Web Development","Little/No Experience":"4","Some Experience":"4","Very Familiar":"2"}',
+    '{"Type of Experience":"Using Git to track changes and share code (add, commit, push, pull)","Little/No Experience":"2","Some Experience":"5","Very Familiar":"3"}'
+  ]
+
+  function verify (err, feedId) {
+    if (err) { console.log(err) }
+    var rs = jawn.core.createReadStream(feedId)
+    rs.on('data', function (block) {
+      t.same(block.toString(), expected.shift(), 'block matches imported line')
+    })
+    t.same(jawn.core.get(feedId).blocks, 4, 'correct number of blocks returned')
+    t.end()
+  }
+})
+
+// helpers
+
+function fixture (name) {
+  return path.join(__dirname, 'data', name)
+}
 
 function freshJawn () {
   return new Jawn({db: memdb()})
+}
+
+function importFromFile (jawn, file, opts, callback) {
+  var importPipeline = jawn.createImportPipeline(opts, callback)
+  var data = fs.createReadStream(fixture(file))
+  data.pipe(importPipeline)
 }
