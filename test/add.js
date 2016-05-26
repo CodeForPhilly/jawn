728b358f10da75beaaffc92e454a87dfaeacf17e
Brandon Mellus <mellusbrandon@gmail.com>
2016-05-26 19:54:15 +0000

    Make add.js in lib and create a test

--- test/add.js
+++ test/add.js
@@ -4,25 +4,29 @@ var memdb = require('memdb')
 
 test('add 3 rows', function (t) {
   var jawn = freshJawn()
+
   var testValues = [
     '{"foo":"bar","name":"josie","age":"35"}',
     '{"foo":"baz","name":"eloise","age":"71"}',
     '{"foo":"baz","name":"francoise","age":"5"}'
   ]
-  /*
+
   var expected = [
     '0: {"foo":"bar","name":"josie","age":"35"}',
     '1: {"foo":"baz","name":"eloise","age":"71"}',
     '2: {"foo":"baz","name":"francoise","age":"5"}'
   ]
-  */
 
-  for (var i = 0; testValues.length; i++) {
+  for (var i = 0; i < testValues.length; i++) {
     jawn.addRow(i, testValues[i])
   }
 
   jawn.kv.on('put', function (key, value, node) {
-    console.log(key + ': ' + value)
+    t.equal(key + ': ' + value, expected.shift(), key + ' is the right value')
+
+    if (expected.length === 0) {
+      t.end()
+    }
   })
 })
 
